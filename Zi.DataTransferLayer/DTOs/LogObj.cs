26f29f5abb4d4a9cc9e6c975d64900ef3fb048ed
zi1611061206 <sotayvaodoi@gmail.com>
2021-09-27 04:30:55 +0700

    Move to and Fix DTOs/LogObj.cs

---
+++ Zi.DataTransferLayer/DTOs/LogObj.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DatabaseEntity.Entities;
+
+namespace DataTransferLayer.DTOs
+{
+    public class LogObj : Log
+    {
+        public LogObj(Guid userId, Guid eventId)
+        {
+            LogId = Guid.NewGuid();
+            UserId = userId;
+            EventId = eventId;
+        }
+
+        /// <summary>
+        /// Mapping data to LogObj
+        /// </summary>
+        public LogObj(DataRow row)
+        {
+            LogId = Guid.Parse(row["LogId"].ToString());
+            UserId = Guid.Parse(row["UserId"].ToString());
+            EventId = Guid.Parse(row["EventId"].ToString());
+            if (DateTime.TryParse(row["Time"].ToString(), out DateTime result))
+            {
+                Time = result;
+            }
+        }
+    }
+}
