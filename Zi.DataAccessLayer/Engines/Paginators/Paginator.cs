98045ee17864ca343afe0e01ece06bbfde546246
zi1611061206 <sotayvaodoi@gmail.com>
2021-09-30 01:55:59 +0700

    Add Engines

---
+++ Zi.DataAccessLayer/Engines/Paginators/Paginator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DataAccessLayer.Engines.Searchers;
+
+namespace Zi.DataAccessLayer.Engines.Paginators
+{
+    public class Paginator : Searcher
+    {
+        public int PageSize { get; set; }
+        public int CurrentPageIndex { get; set; }
+        public int TotalRecords { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((double)TotalRecords / PageSize);
+            }
+        }
+        public int TotalRecordsLastPage
+        {
+            get
+            {
+                return TotalRecords - PageSize * (TotalPages - 1);
+            }
+        }
+
+        public Paginator(int pageSize = 25, int currentPageIndex = 1)
+        {
+            PageSize = pageSize;
+            CurrentPageIndex = currentPageIndex;
+        }
+    }
+}
---
+++ Zi.DataAccessLayer/Engines/Searchers/Searcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zi.DataAccessLayer.Engines.Searchers
+{
+    public class Searcher
+    {
+        public string Keyword { get; set; }
+
+        public Searcher()
+        {
+            Keyword = string.Empty;
+        }
+    }
+}
