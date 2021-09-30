c6c54d205599a1da5d5065e6825b1d770d742642
zi1611061206 <sotayvaodoi@gmail.com>
2021-10-01 06:23:28 +0700

    LinqToEntityLayer add usecase: Category

---
+++ Zi.LinqToEntityLayer/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DatabaseEntity.Entities;
+using Zi.LinqToEntityLayer.Engines.Filters;
+using Zi.LinqToEntityLayer.Engines.Paginators;
+
+namespace Zi.LinqToEntityLayer.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Paginator<Category> GetCategories(CategoryFilter filter);
+        Task<bool> AddCategory(Category category);
+        Task<bool> UpdateCategory(Category category);
+        Task<bool> DeleteCategory(Guid categoryId);
+    }
+}
