ffb457dfacd11f4420ab925924e909dbaf102eef
zi1611061206 <sotayvaodoi@gmail.com>
2021-10-01 07:20:24 +0700

    LinqToEntityLayer add usecase: Table

---
+++ Zi.LinqToEntityLayer/Engines/Filters/TableFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DatabaseEntity.Enumerators;
+using Zi.LinqToEntityLayer.Engines.Paginators;
+
+namespace Zi.LinqToEntityLayer.Engines.Filters
+{
+    public class TableFilter : PaginatorConfiguration
+    {
+        public Guid TableId { get; set; }
+        public TableStatus? Status { get; set; }
+        public Guid AreaId { get; set; }
+
+        public TableFilter()
+        {
+            TableId = Guid.Empty;
+            Status = null;
+            AreaId = Guid.Empty;
+        }
+    }
+}
---
+++ Zi.LinqToEntityLayer/Services/Interfaces/ITableService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DatabaseEntity.Entities;
+using Zi.LinqToEntityLayer.Engines.Filters;
+using Zi.LinqToEntityLayer.Engines.Paginators;
+
+namespace Zi.LinqToEntityLayer.Services.Interfaces
+{
+    public interface ITableService
+    {
+        Paginator<Table> GetTables(TableFilter filter);
+        Task<bool> AddTable(Table table);
+        Task<bool> UpdateTable(Table table);
+        Task<bool> DeleteTable(Guid tableId);
+    }
+}
