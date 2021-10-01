66562bf961777f509a3b647a6cefa19877b51528
zi1611061206 <sotayvaodoi@gmail.com>
2021-10-01 09:54:44 +0700

    LinqToEntityLayer add usecase: PromotionType

---
+++ Zi.LinqToEntityLayer/Engines/Filters/PromotionTypeFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.LinqToEntityLayer.Engines.Paginators;
+
+namespace Zi.LinqToEntityLayer.Engines.Filters
+{
+    public class PromotionTypeFilter : PaginatorConfiguration
+    {
+        public Guid PromotionTypeId { get; set; }
+
+        public PromotionTypeFilter()
+        {
+            PromotionTypeId = Guid.Empty;
+        }
+    }
+}
---
+++ Zi.LinqToEntityLayer/Services/PromotionTypeService.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zi.DatabaseEntity;
+using Zi.DatabaseEntity.Entities;
+using Zi.LinqToEntityLayer.Engines.Filters;
+using Zi.LinqToEntityLayer.Engines.Paginators;
+using Zi.LinqToEntityLayer.Services.Interfaces;
+
+namespace Zi.LinqToEntityLayer.Services
+{
+    public class PromotionTypeService : IPromotionTypeService
+    {
+        public async Task<bool> AddPromotionType(PromotionType promotionType)
+        {
+            using (var context = new ZiDbContext())
+            {
+                context.PromotionTypes.Add(promotionType);
+                if (await context.SaveChangesAsync() <= 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public async Task<bool> DeletePromotionType(Guid promotionTypeId)
+        {
+            using (var context = new ZiDbContext())
+            {
+                var promotionType = await context.PromotionTypes.FindAsync(promotionTypeId);
+                context.PromotionTypes.Remove(promotionType);
+                if (await context.SaveChangesAsync() <= 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public Paginator<PromotionType> GetAreaPromotionTypes(PromotionTypeFilter filter)
+        {
+            using (var context = new ZiDbContext())
+            {
+                var query = context.PromotionTypes;
+                query = GettingBy(query, filter);
+                //query = Filtering(query, filter);
+                query = Searching(query, filter);
+                query = Paging(query, filter);
+                query = Sorting(query, filter);
+                // Mapping data
+                var data = query.Select(x => new PromotionType()
+                {
+                    PromotionTypeId = x.PromotionTypeId,
+                    Name = x.Name,
+                    Description = x.Description
+                });
+                var result = new Paginator<PromotionType>()
+                {
+                    TotalRecords = data.Count(),
+                    PageSize = filter.PageSize,
+                    CurrentPageIndex = filter.CurrentPageIndex,
+                    Item = data.ToList()
+                };
+                return result;
+            }
+        }
+
+        #region Engines
+        private DbSet<PromotionType> GettingBy(DbSet<PromotionType> query, PromotionTypeFilter filter)
+        {
+            if (filter.PromotionTypeId.CompareTo(Guid.Empty) != 0)
+            {
+                query.Where(x => x.PromotionTypeId.CompareTo(filter.PromotionTypeId) == 0);
+            }
+            return query;
+        }
+
+        private DbSet<PromotionType> Searching(DbSet<PromotionType> query, PromotionTypeFilter filter)
+        {
+            if (!string.IsNullOrEmpty(filter.Keyword))
+            {
+                if (filter.IsRoughly)
+                {
+                    query.Where(x => x.PromotionTypeId.ToString().Contains(filter.Keyword) ||
+                        x.Name.ToString().Contains(filter.Keyword) ||
+                        x.Description.ToString().Contains(filter.Keyword));
+                }
+                else
+                {
+                    query.Where(x => x.PromotionTypeId.ToString().Equals(filter.Keyword) ||
+                        x.Name.ToString().Equals(filter.Keyword) ||
+                        x.Description.ToString().Equals(filter.Keyword));
+                }
+            }
+            return query;
+        }
+
+        private DbSet<PromotionType> Paging(DbSet<PromotionType> query, PromotionTypeFilter filter)
+        {
+            int firstIndexOfPage = (filter.CurrentPageIndex - 1) * filter.PageSize;
+            query.Skip(firstIndexOfPage).Take(filter.PageSize);
+            return query;
+        }
+
+        private DbSet<PromotionType> Sorting(DbSet<PromotionType> query, PromotionTypeFilter filter)
+        {
+            if (filter.IsAscending)
+            {
+                query.OrderBy(x => x.Name);
+            }
+            else
+            {
+                query.OrderByDescending(x => x.Name);
+            }
+            return query;
+        }
+        #endregion
+
+        public async Task<bool> UpdatePromotionType(PromotionType promotionType)
+        {
+            using (var context = new ZiDbContext())
+            {
+                var data = await context.PromotionTypes.FindAsync(promotionType.PromotionTypeId);
+                data.Name = promotionType.Name;
+                data.Description = promotionType.Description;
+                if (await context.SaveChangesAsync() <= 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
