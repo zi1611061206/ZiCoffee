3b0d1e68cf3ef63a5650aa95b5cf6f409115e516
Nguyen Ngoc Hieu <hieunn23@fpt.com.vn>
2023-03-07 23:10:29 +0700

    update README.md

---
+++ Zi.DatabaseEntity/Migrations/202212071708437_test.cs
@@ -0,0 +1,16 @@
+﻿namespace Zi.DatabaseEntity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
